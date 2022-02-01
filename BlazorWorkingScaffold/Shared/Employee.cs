2d5b4bbc03d0f2b8be2dabd87edcb1141b008fb2
xsdrt <m_redinger@hotmail.com>
2022-02-01 13:13:14 -0500

    Initialzed a DevBranch just now to develop with.  added some comments to the Employye Model (Employee.cs) on how to install Enity Framework core 6.

--- BlazorWorkingScaffold/Shared/Employee.cs
+++ BlazorWorkingScaffold/Shared/Employee.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace BlazorWorkingScaffold.Shared
 {
-    public class Employee       //Employee Model
+    public class Employee       //Employee Model        //Make sure and install via package manager Enity Framework core 6
+                                //using the following command (dotnet tool install --global dotnet-ef).   If you already
+                                //have installed you can update to the latest core 6 by the following command
+                                //(dotnet tool uninstall --global dotnet-ef) and then reinstall
+                                //(dotnet tool install --global dotnet-ef) 
     {
         public int Id { get; set; }
 
