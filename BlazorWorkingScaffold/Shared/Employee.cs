248463b473b88d6fcba0a7059ee63f4012d3f02b
xsdrt <m_redinger@hotmail.com>
2022-02-01 13:05:16 -0500

    Added a Employye model.  Also installed Enity Core 6 using the packaage manager (dotnet tool install --global dotnet-ef).

--- BlazorWorkingScaffold/Shared/Employee.cs
+++ BlazorWorkingScaffold/Shared/Employee.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace BlazorWorkingScaffold.Shared
 {
-    public class Employee
+    public class Employee       //Employee Model
     {
         public int Id { get; set; }
 
         public string FirstName { get; set; } = string.Empty; //new in .net6 and C#10
         public string LastName { get; set; } = String.Empty;
-        public string Posistion { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
     }
 }
