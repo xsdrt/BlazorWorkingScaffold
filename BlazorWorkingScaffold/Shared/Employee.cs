c5a15bd7fbfded4166703de910a3bcf932d8f13c
xsdrt <m_redinger@hotmail.com>
2022-02-01 12:48:20 -0500

    Add project files.

---
+++ BlazorWorkingScaffold/Shared/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWorkingScaffold.Shared
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty; //new in .net6 and C#10
+        public string LastName { get; set; } = String.Empty;
+        public string Posistion { get; set; } = string.Empty;
+    }
+}
