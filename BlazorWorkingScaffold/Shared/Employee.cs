75160db9a65ed0899d305145e69f9add40f43aa4
xsdrt <m_redinger@hotmail.com>
2022-02-01 13:32:20 -0500

    Installed the following nuget packagaes for Entity: Mcrosoft.EntityFrameworkCore 6.01 : Microsoft.EntityFrameworkCore.Design 6.01 : Microsoft.EntityFrameworkCore.SqlServer 6.01 .

--- BlazorWorkingScaffold/Shared/Employee.cs
+++ BlazorWorkingScaffold/Shared/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlazorWorkingScaffold.Shared
 {
-    public class Employee       //Employee Model        //Make sure and install via package manager Enity Framework core 6
+    public class Employee       //Employee Model        //Make sure and install via package manager Entity Framework Core 6
                                 //using the following command (dotnet tool install --global dotnet-ef).   If you already
                                 //have installed you can update to the latest core 6 by the following command
                                 //(dotnet tool uninstall --global dotnet-ef) and then reinstall
